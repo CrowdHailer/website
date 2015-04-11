f9a4589d52437a2d26075ff84acd0de05aeae259
Peter Saxton <peterhsaxton@gmail.com>
2015-04-11 11:43:47 +0100

    add minify step

--- tasks/build.js
+++ tasks/build.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
+var minifyHTML = require('gulp-minify-html');
 
 gulp.task('clean', function () {
   return del.sync(['www']);
@@ -7,6 +8,7 @@ gulp.task('clean', function () {
 
 gulp.task('copy', function () {
   gulp.src('./public/**/*', {base: './public'})
+  .pipe(minifyHTML({}))
   .pipe(gulp.dest('./www'));
 });
 
