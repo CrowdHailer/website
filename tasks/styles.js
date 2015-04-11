5de1106f1b153c1c21f49fb18aba5b859f47b073
Peter Saxton <peterhsaxton@gmail.com>
2015-04-11 12:00:19 +0100

    compress css

--- tasks/styles.js
+++ tasks/styles.js
@@ -4,7 +4,7 @@ var prefix = require('gulp-autoprefixer');
 
 gulp.task('styles', function () {
     return gulp.src('assets/styles/main.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(prefix())
         .pipe(gulp.dest('www/styles'));
 });
