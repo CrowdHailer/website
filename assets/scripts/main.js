dddb62ce2e06fab3d7bf6c9b2185c6085cea8a78
Peter Saxton <peterhsaxton@gmail.com>
2015-04-12 20:37:07 +0100

    js set flipped

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,3 +6,13 @@ if (screenfull.enabled) {
     screenfull.request();
    }, false);
 };
+
+var main = document.querySelector('main')
+var navbars = document.querySelectorAll('nav');
+// var a = ["a", "b", "c"];
+[navbars[0], navbars[1]].forEach(function(element) {
+    element.onclick = function(e){
+
+      main.classList.toggle('hover')
+    }
+});
