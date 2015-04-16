37dc3b7b3f70e3e5e2c16dd0e9ac9c9355c90d9b
Peter Saxton <peterhsaxton@gmail.com>
2015-04-16 18:33:16 +0100

    inject css

--- tasks/build.js
+++ tasks/build.js
@@ -1,18 +1,27 @@
 var gulp = require('gulp');
 var del = require('del');
 var minifyHTML = require('gulp-minify-html');
+var inject = require('gulp-inject');
 
 gulp.task('clean', function () {
   return del.sync(['www']);
 });
 
+function fileContents(filePath, file) {
+    return file.contents.toString('utf8');
+}
+
 gulp.task('copy', function () {
+  var sources = gulp.src(['./src/**/*.js', './www/**/*.css']);
+  console.log('boo')
+
   gulp.src('./public/**/*', {base: './public'})
   // .pipe(minifyHTML({}))
+  .pipe(inject(sources, { transform: fileContents }))
   .pipe(gulp.dest('./www'));
 });
 
-gulp.task('build', ['clean', 'copy', 'styles', 'scripts'])
+gulp.task('build', ['clean', 'styles', 'scripts', 'copy'])
 
 gulp.task('watch', function () {
   var core_watcher = gulp.watch('./public/**/*', ['copy'])
