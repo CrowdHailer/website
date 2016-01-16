9389d65e0ffc86c1a60b173146a0385fe40e9d12
CrowdHailer <peterhsaxton@gmail.com>
2016-01-16 11:50:37 +0000

    fetch scut dependency

--- tasks/fetch.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var download = require("gulp-download");
-
-gulp.task('fetch', function () {
-  download('https://raw.githubusercontent.com/davidtheclark/scut/master/dist/_scut.scss')
-      .pipe(gulp.dest("assets/styles/vendor"));
-})
