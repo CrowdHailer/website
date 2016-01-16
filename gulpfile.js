9c86690f092545e93d2bb86ee3dbaabbccb0500c
CrowdHailer <peterhsaxton@gmail.com>
2016-01-16 11:53:52 +0000

    clean up tasks

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp')
-var requireDir = require('require-dir');
-
-// Require all tasks as separate directory files
-var dir = requireDir('./tasks');
-
-gulp.task('hello', function() {
-  console.log('Hello!')
-});
